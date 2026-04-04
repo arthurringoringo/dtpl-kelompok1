c9a1092b0d8021147f5e6177bd0fdedfd9228824
Arthur Boi Paraja <arthur.ringoringo@gmail.com>
2026-04-04 20:54:42 +0700

    Update main-layout.tsx

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,11 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import "../pages/landing-page/landing-page.css"; // supaya styling nav+footer kebawa
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+  }, [pathname]);
+  return null;
+}
+
 export default function MainLayout() {
   return (
     <div className="appShell">
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
